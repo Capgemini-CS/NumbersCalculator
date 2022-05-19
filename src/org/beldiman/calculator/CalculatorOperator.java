a848b79667a7672d21a0ee6244455701c371ea51
sbeldima <stefan.beldiman@capgemini.com>
2022-05-19 17:33:23 +0300

    Switch  case remove and enum implemented

--- src/org/beldiman/calculator/CalculatorOperator.java
+++ src/org/beldiman/calculator/CalculatorOperator.java
@@ -3,26 +3,9 @@ package org.beldiman.calculator;
 import org.beldiman.calculations.*;
 
 public class CalculatorOperator implements OperatorInt{
-//    OperationBase adder = new Adder();
-//    OperationBase subtracter = new Subtracter();
-//    OperationBase multiplier = new Multiplier();
-//    OperationBase divider = new Divider();
 
     public double calculate (int firstNumber, int secondNumber, char operation) {
         return OperatorEnum.getOperatorEnumForOperand(operation).performCalculation(firstNumber, secondNumber);
-//        switch (operation) {
-//            case '+':
-//                return adder.doOperation(firstNumber, secondNumber);
-//            case '-':
-//                return subtracter.doOperation(firstNumber, secondNumber);
-//            case '*':
-//                return multiplier.doOperation(firstNumber, secondNumber);
-//            case '/':
-//                if (secondNumber == 0) {
-//                    throw new ArithmeticException("Cannot divide by 0!");
-//                }
-//                return divider.doOperation(firstNumber, secondNumber);
-//        }
-//        return 0;
+
     }
 }
